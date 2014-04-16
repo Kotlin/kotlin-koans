95bd22ac5f6c983fafc15fe6072be03c6677614b
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2014-04-16 18:35:14 +0400

    Replaced additional tasks on builders with questionnaire

---
+++ src/iv_builders/_20_BuildersHowItWorks.kt
@@ -0,0 +1,98 @@
+package iv_builders
+
+import java.util.HashMap
+import util.TODO
+import iv_builders.htmlLibrary.html
+import com.google.common.collect.Multimap
+import com.google.common.collect.HashMultimap
+import java.util.ArrayList
+import iv_builders.htmlLibrary.*
+import iv_builders.data.getProducts
+import iv_builders.Answer.*
+
+fun todoTask20() = TODO(
+    """
+        Task20.
+        Look at the questions below and give your answers:
+        change 'insertAnswerHere()' in task20's map to your choice (a, b or c).
+    """
+)
+
+fun insertAnswerHere() = todoTask20()
+
+enum class Answer {
+    // all the constants are imported by 'import iv_builders.Answer.*'
+    // so they can be accessed by name
+    a b c
+}
+
+fun task20() = linkedMapOf<Int, Answer>(
+/*
+1. In the Kotlin code
+        tr {
+            td {
+                text("Product")
+            }
+            td {
+                text("Popularity")
+            }
+        }
+'td' is:
+    a. special built-in syntactic construct
+    b. function declaration
+    c. function invocation
+ */
+        1 to insertAnswerHere(),
+
+/*
+2. In the Kotlin code
+            tr (color = "yellow") {
+                td {
+                    text("Product")
+                }
+                td {
+                    text("Popularity")
+                }
+            }
+'color' is:
+  a. new variable declaration
+  b. argument name
+  c. argument value
+ */
+        2 to insertAnswerHere(),
+
+/*
+3. The block
+                 {
+                     text("Product")
+                 }
+from the previous question is:
+  a. block inside built-in syntax construction 'td'
+  b. function literal (or "lambda")
+  c. something mysterious
+
+ */
+        3 to insertAnswerHere(),
+
+/*
+4. For the code
+            tr (color = "yellow") {
+                this.td {
+                    text("Product")
+                }
+                td {
+                    text("Popularity")
+                }
+            }
+which of the following is true:
+  a. this code doesn't compile
+  b. 'this' refers to an instance of an outer class
+  c. 'this' refers to a receiver parameter TR of the function literal:
+            tr (color = "yellow") { TR.(): Unit ->
+                  this.td {
+                      text("Product")
+                  }
+            }
+ */
+        4 to insertAnswerHere()
+)
--- test/iv_builders/_20_Builders_How_It_Works.kt
+++ test/iv_builders/_20_Builders_How_It_Works.kt
@@ -2,26 +2,11 @@ package iv_builders
 
 import junit.framework.Assert
 import org.junit.Test as test
-import java.util.HashMap
-import com.google.common.collect.HashMultimap
+import iv_builders.Answer.*
 
 class _20_Builders_How_It_Works {
-    test fun testMapBuilder() {
-        val map = createMapWithMapBuilder()
-        val expected = HashMap<Int, String>()
-        for (i in 0..10) {
-            expected[i] = "$i"
-        }
-        Assert.assertEquals("Map should be filled with the right values", expected, map)
-    }
-
-    test fun testMultiMapBuilder() {
-        val multiMap = createMultiMapWithMapBuilder()
-        val expected = HashMultimap.create<Int, String>()!!
-        expected.put(0, "")
-        for (i in 0..9) {
-            expected.put(1, "$i")
-        }
-        Assert.assertEquals("MultiMap should be filled with the right values", expected, multiMap)
+    test fun testQuestionnaire() {
+        val answers = linkedMapOf(1 to c, 2 to b, 3 to b, 4 to c)
+        Assert.assertEquals("Your answers are incorrect: ", answers, task20())
     }
 }
