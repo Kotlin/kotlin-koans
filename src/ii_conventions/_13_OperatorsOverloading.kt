000c217c3b290a697b5d04bd6a4962062ed999c5
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2014-04-16 16:00:26 +0400

    Added tips for task 13

--- src/ii_conventions/_13_OperatorsOverloading.kt
+++ src/ii_conventions/_13_OperatorsOverloading.kt
@@ -6,21 +6,22 @@ import ii_conventions.TimeInterval.*
 fun infixNotation() {
     fun Int.foo(s: String) = "$s$this"
 
-    1.foo("abc")
     1 foo "abc"
+    // the same as:
+    1.foo("abc")
 }
 
 //Only predefined operators can be overloaded by the corresponding names
 //A complete list of operators can be found in syntax/OperatorOverloading.kt
 trait A {
     fun plus(a: A): A
-    fun minus(a: A): A
+    fun times(a: A): A
     fun not(): A
 }
 
 fun use(a1: A, a2: A) {
     a1 + a2
-    a1 - a2
+    a1 * a2
     !a1
 }
 
@@ -31,9 +32,12 @@ fun todoTask13() = TODO(
         Use classes MyDate and TimeInterval.
         Use a utility function MyDate.addTimeIntervals.
         Uncomment the commented line and make it compile.
-        In a task 13_2 you may need to add an extra class (or use Pair).
+
+        (1). Add an extension function 'plus()' to MyDate, taking TimeInterval as an argument.
+        (2). Support adding several time intervals to a date. Add an extra class.
+        If any problems, see ii_conventions/_13_Tips.kt.
     """,
-    references = { (date: MyDate, timeInterval: TimeInterval, syntax: syntax.operatorOverloading.B) ->
+    references = { (date: MyDate, timeInterval: TimeInterval) ->
         date.addTimeIntervals(timeInterval, 1)
     })
 
---
+++ src/ii_conventions/_13_Tips.kt
@@ -0,0 +1,14 @@
+package ii_conventions
+
+import util.TODO
+
+fun todoTask13_2() = TODO(
+    """
+        Task13.2.
+        Support adding several time intervals to a date.
+        Add an extra class for storing the time interval and the number of intervals,
+        e.g. 'class RepeatedTimeInterval(val ti: TimeInterval, val n: Int)'.
+        Add an extension function 'times' to TimeInterval, constructing the value of this class.
+        Add an extension function 'plus' to MyDate, taking RepeatedTimeInterval as an argument.
+    """
+)
