e8540f28898ce9138a77b1cbd5ac92a4f27ee8af
Hadi Hariri <mail@hadihariri.com>
2014-05-20 12:47:27 +0200

    Fixed some minor grammar mistakes

--- src/i_introduction/_0_Hello_World/HelloWorld.kt
+++ src/i_introduction/_0_Hello_World/HelloWorld.kt
@@ -10,16 +10,16 @@ fun main(args: Array<String>) {
 fun todoTask0() = TODO(
     """
         Introduction.
-        The task in the workshop is usually to change the function 'taskN' by replacing it's body,
+        The task in the workshop is usually to change the function 'taskN' by replacing its body,
         the function invocation 'todoTaskN()', with the corresponding code according to the problem.
         The function 'todoTaskN()' throws an exception, so you usually have to replace its invocation with meaningful code.
 
-        There are usually explanations of the Kotlin features to read at the same file as the task.
-        Please read them, we prepared carefully them for you =).
+        There are usually explanations of the Kotlin features to use in the same file as the task.
+        Please read them, we prepared them carefully for you =).
 
         Using references you can usually navigate and see the code mentioned in the task description.
 
-        For the start please make the function 'task0' return "OK" =).
+        To start please make the function 'task0' returns "OK" =).
     """,
     references = { task0(); "OK" }
 )
