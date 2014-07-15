d01da016e20a7f1c3eaf09cc8e125a59a731c972
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2014-07-15 22:06:42 +0400

    Update to M8

--- src/util/kotlinUtil.kt
+++ src/util/kotlinUtil.kt
@@ -3,4 +3,4 @@ package util
 suppress("UNUSED_PARAMETER")
 fun TODO(task: String, vararg references: Any?) = throw NotImplementedException("TODO: $task")
 
-class NotImplementedException(val message: String): Exception(message)
+class NotImplementedException(message: String): Exception(message)
