9bcfa886ec88d1b4d5378676040eb7f093aaf915
satamas <se.atamas@gmail.com>
2015-03-02 14:59:17 +0300

    Move todoCollectionTask to todoUtil.kt

--- src/v_collections/A_Introduction_.kt
+++ src/v_collections/A_Introduction_.kt
@@ -38,12 +38,3 @@ fun Shop.getSetOfCustomers(): Set<Customer> {
 //    return this.customers
 }
 
-fun todoCollectionTask() = TODO(
-        """
-           Task on working with collections.
-           Look through 'Shop' API (all tasks are connected with it).
-           Return what is described in the name and the comment.
-
-        """,
-        references = {(shop: Shop) -> shop.customers }
-)
