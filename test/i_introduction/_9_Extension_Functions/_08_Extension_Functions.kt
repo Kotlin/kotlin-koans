ddef36bfed13d630a0915a31c64d180393201df7
Chris Stewart <cstew415@gmail.com>
2015-12-11 14:29:40 -0500

    Fixed the name of two test classes that had the wrong numbering

--- test/i_introduction/_9_Extension_Functions/_08_Extension_Functions.kt
+++ test/i_introduction/_9_Extension_Functions/_08_Extension_Functions.kt
@@ -3,7 +3,7 @@ package i_introduction._9_Extension_Functions
 import org.junit.Assert
 import org.junit.Test
 
-class _08_Extension_Functions() {
+class _09_Extension_Functions() {
     @Test fun testIntExtension() {
         Assert.assertEquals("Rational number creation error: ", RationalNumber(4, 1), 4.r())
     }
