6f285664739acf20356adc3436a9c9471981ec71
Kevin Dorff <kevin.dorff@rackspace.com>
2016-02-18 11:55:36 -0600

    Started writing the task TODO descriptions so that make more sense to me. Will provide a sample for a PR to see if the original author concurs.

--- src/i_introduction/_3_Default_Arguments/DefaultAndNamedParams.kt
+++ src/i_introduction/_3_Default_Arguments/DefaultAndNamedParams.kt
@@ -7,9 +7,13 @@ fun todoTask3(): Nothing = TODO(
     """
         Task 3.
         Several overloads of 'JavaCode3.foo()' can be replaced with one function in Kotlin.
-        Change the declaration of the function 'foo' in a way that makes the code using 'foo' compile.
-        You have to add parameters and replace 'todoTask3()' with a real body.
-        Uncomment the commented code and make it compile.
+
+        The task3() function contents must be modified to remove the line "return todoTask3()"
+        and un-comment the four lines starting with "return (foo("A") +".
+
+        The goal of this task is to change the list of arguments for the function 'foo', provided
+        just below, and replace the function implementation such that the code in task3() will return
+        the String "a42b1C42D2".
     """,
     documentation = doc2(),
     references = { name: String -> JavaCode3().foo(name); foo(name) })
@@ -17,9 +21,9 @@ fun todoTask3(): Nothing = TODO(
 fun foo(name: String): String = todoTask3()
 
 fun task3(): String {
-    todoTask3()
-//    return (foo("a") +
-//            foo("b", number = 1) +
-//            foo("c", toUpperCase = true) +
-//            foo(name = "d", number = 2, toUpperCase = true))
+    return todoTask3()
+    //return (foo("a") +
+    //        foo("b", number = 1) +
+    //        foo("c", toUpperCase = true) +
+    //        foo(name = "d", number = 2, toUpperCase = true))
 }
