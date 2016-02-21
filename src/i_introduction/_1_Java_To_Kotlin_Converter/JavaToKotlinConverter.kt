4c1f92c49b247631abd9d7144c085301b5cbdf9c
Eugene Toporov <eugene.toporov@gmail.com>
2016-02-21 13:05:22 +0100

    IntelliJ -> IntelliJ IDEA

--- src/i_introduction/_1_Java_To_Kotlin_Converter/JavaToKotlinConverter.kt
+++ src/i_introduction/_1_Java_To_Kotlin_Converter/JavaToKotlinConverter.kt
@@ -6,7 +6,7 @@ fun todoTask1(collection: Collection<Int>): Nothing = TODO(
     """
         Task 1.
         Rewrite JavaCode1.task1 in Kotlin.
-        In IntelliJ, you can just copy-paste the code and agree to automatically convert it to Kotlin,
+        In IntelliJ IDEA, you can just copy-paste the code and agree to automatically convert it to Kotlin,
         but only for this task!
     """,
     references = { JavaCode1().task1(collection) })
