d62f9be35c993f6855e8bc74c56fff7de2e48c53
icejoywoo <icejoywoo@icejoywoodeMacBook-Pro.local>
2017-03-10 18:21:54 +0800

    fix MyDate.toMillis: set hour, minute, second & microsecond to 0

--- test/iv_properties/_35_Delegates_How_It_Works.kt
+++ test/iv_properties/_35_Delegates_How_It_Works.kt
@@ -11,5 +11,7 @@ class _35_Delegates_How_It_Works {
         assertEquals(2014, d.date.year)
         assertEquals(1, d.date.month)
         assertEquals(13, d.date.dayOfMonth)
+        assertEquals(d.date, 1392220800000.toDate())
+        assertEquals(1392220800000, d.date.toMillis())
     }
 }
