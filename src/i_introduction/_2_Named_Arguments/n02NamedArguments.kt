0ed4ba19d1da556cc664252c4cc97fae01218ce2
roberto.almaraz <roberto.almaraz@habilgroup.net>
2017-11-10 16:47:46 -0600

    :sparkles: Soluciona i_3

--- src/i_introduction/_2_Named_Arguments/n02NamedArguments.kt
+++ src/i_introduction/_2_Named_Arguments/n02NamedArguments.kt
@@ -12,18 +12,6 @@ fun usage() {
     bar(1, b = false)
 }
 
-fun todoTask2(): Nothing = TODO(
-    """
-        Task 2.
-        Print out the collection contents surrounded by curly braces using the library function 'joinToString'.
-        Specify only 'prefix' and 'postfix' arguments.
-
-        Don't forget to remove the 'todoTask2()' invocation which throws an exception.
-    """,
-    documentation = doc2(),
-    references = { collection: Collection<Int> -> task1(collection); collection.joinToString() })
-
 fun task2(collection: Collection<Int>): String {
-    todoTask2()
-    return collection.joinToString()
+    return collection.joinToString(prefix = "{", postfix = "}")
 }
