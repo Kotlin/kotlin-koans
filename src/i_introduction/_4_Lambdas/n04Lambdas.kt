b62bb56564a6d83562eefc6e0d7a8f739f47eb8d
roberto.almaraz <roberto.almaraz@habilgroup.net>
2017-11-10 17:19:13 -0600

    :sparkles: Soluciona i_4

--- src/i_introduction/_4_Lambdas/n04Lambdas.kt
+++ src/i_introduction/_4_Lambdas/n04Lambdas.kt
@@ -11,15 +11,4 @@ fun example() {
     sum(1, square(2)) == 5
 }
 
-fun todoTask4(collection: Collection<Int>): Nothing = TODO(
-    """
-        Task 4.
-        Rewrite 'JavaCode4.task4()' in Kotlin using lambdas:
-        return true if the collection contains an even number.
-        You can find the appropriate function to call on 'Collection' by using code completion.
-        Don't use the class 'Iterables'.
-    """,
-    documentation = doc4(),
-    references = { JavaCode4().task4(collection) })
-
-fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
+fun task4(collection: Collection<Int>): Boolean = collection.stream().anyMatch { element -> element!! % 2 == 0 };
