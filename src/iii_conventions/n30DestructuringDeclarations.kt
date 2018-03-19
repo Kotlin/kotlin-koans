c1996884c882752c261e375941c2ce67aee3fc84
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2018-03-19 13:48:59 +0100

    Added comment of the correct 'leap' year definition after fix

--- src/iii_conventions/n30DestructuringDeclarations.kt
+++ src/iii_conventions/n30DestructuringDeclarations.kt
@@ -17,8 +17,9 @@ fun isLeapDay(date: MyDate): Boolean {
     todoTask30()
 //    val (year, month, dayOfMonth) = date
 //
-//    fun isLeapYear(year: Int): Boolean = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
-//
 //    // 29 February of a leap year
 //    return isLeapYear(year) && month == 2 && dayOfMonth == 29
 }
+
+// Years which are multiples of four (with the exception of years divisible by 100 but not by 400)
+fun isLeapYear(year: Int): Boolean = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
