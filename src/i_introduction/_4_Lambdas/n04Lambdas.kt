28966e090ed6da742f9006ae52e8c9f5205d26cf
Josef <vavra.pepa@gmail.com>
2018-11-01 19:29:59 +0100

    Task 4 resolved

--- src/i_introduction/_4_Lambdas/n04Lambdas.kt
+++ src/i_introduction/_4_Lambdas/n04Lambdas.kt
@@ -22,4 +22,6 @@ fun todoTask4(collection: Collection<Int>): Nothing = TODO(
     documentation = doc4(),
     references = { JavaCode4().task4(collection) })
 
-fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
+fun task4(collection: Collection<Int>): Boolean {
+    return collection.any { numb -> numb % 2 == 0 }
+}
